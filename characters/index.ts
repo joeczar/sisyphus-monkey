dfbdee71107dd3c28dc5f5631bba84cf74e3210f
Joe Czarnecki <joe.czar@outlook.com>
2024-02-22 11:18:28 +0100

    chore: added charCoutn, packetNr to message data

--- characters/index.ts
+++ characters/index.ts
@@ -3,16 +3,18 @@ import path from 'path';
 import WebSocket from 'ws';
 
 const wsServers = {
-  word: "ws://192.168.178.138:8080",
-  mac: 'ws://192.168.178.126:8080'
-}
+  word: 'ws://192.168.178.138:8080',
+  mac: 'ws://192.168.178.126:8080',
+};
 
 const FOLDER_PATH = path.resolve(__dirname, '../generated-letters-chunked');
 
 const ws = new WebSocket(wsServers.word);
 
+let packetNr = 0;
+
 // Thresholds for backpressure handling
-const HIGH_WATER_MARK =  64 * 1024; // 64KB
+const HIGH_WATER_MARK = 64 * 1024; // 64KB
 const BACKPRESSURE_THRESHOLD = HIGH_WATER_MARK / 4; // Resume reading when bufferedAmount falls below this
 
 ws.on('open', function open() {
@@ -29,18 +31,29 @@ ws.on('error', function error(err) {
 
 async function processFileLetterByLetter(filePath: string) {
   return new Promise<void>((resolve, reject) => {
-    const readStream = fs.createReadStream(filePath, { encoding: 'utf8', highWaterMark: HIGH_WATER_MARK });
+    const readStream = fs.createReadStream(filePath, {
+      encoding: 'utf8',
+      highWaterMark: HIGH_WATER_MARK,
+    });
 
-    setInterval(()=>{
+    setInterval(() => {
       readStream.on('data', (chunk) => {
         // Send data only if WebSocket's bufferedAmount is below the threshold
         if (ws.bufferedAmount < BACKPRESSURE_THRESHOLD) {
+          // count chars in chunk
+          const charCount = chunk.toString().split('').length;
+          const packet = {
+            chunk: chunk.toString(),
+            charCount,
+            packetNr,
+          };
           ws.send(chunk.toString());
-          console.log(chunk)
+          console.log(packet, chunk);
+          packetNr++;
         } else {
           // Pause reading if we hit the threshold
           readStream.pause();
-          
+
           // Check bufferedAmount and resume reading when it decreases below threshold
           const interval = setInterval(() => {
             if (ws.bufferedAmount < BACKPRESSURE_THRESHOLD) {
@@ -50,8 +63,7 @@ async function processFileLetterByLetter(filePath: string) {
           }, 100); // Check every 100ms
         }
       });
-    }, 50)
-    
+    }, 50);
 
     readStream.on('end', () => {
       // console.log(`Finished processing file: ${filePath}`);
@@ -69,7 +81,8 @@ async function processFolder(folderPath: string) {
   const files = await fs.promises.readdir(folderPath);
   for (const fileName of files) {
     const filePath = path.join(folderPath, fileName);
-    if (fs.statSync(filePath).isFile()) { // Ensure you're reading files
+    if (fs.statSync(filePath).isFile()) {
+      // Ensure you're reading files
       console.log(`Processing file: ${fileName}`);
       await processFileLetterByLetter(filePath);
     }
