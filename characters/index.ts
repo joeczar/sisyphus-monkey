0d24ed5eafa2327967aaf96127ecdfc418af47a0
Joe Czarnecki <joe.czar@outlook.com>
2024-03-16 13:29:25 +0100

    chore: better packet save query

--- characters/index.ts
+++ characters/index.ts
@@ -1,7 +1,31 @@
-import { getAndParsePackets } from "./readAndSavePackets";
+import { getAndParsePackets } from './readAndSavePackets';
+import { packetService } from '../db/neo4j/PacketService';
 
 async function initializeChars() {
-  await getAndParsePackets()
+  if ((await packetService.checkConnection()) === false) {
+    console.error('Could not connect to Neo4j');
+    // retry 3 times
+    for (let i = 0; i < 3; i++) {
+      console.log('Retrying connection...');
+      if ((await packetService.checkConnection()) === true) {
+        break;
+      }
+      await new Promise((resolve) => setTimeout(resolve, 5000));
+    }
+    // if still not connected, exit
+    if ((await packetService.checkConnection()) === false) {
+      console.error('Could not connect to Neo4j');
+      process.exit(1);
+    }
+  }
+  // get user input to start the process
+  const prompt = 'Press any key to start the process';
+  process.stdout.write(prompt);
+  for await (const line of console) {
+    if (line) {
+      await getAndParsePackets();
+    }
+  }
 }
 
-await initializeChars()
+await initializeChars();
