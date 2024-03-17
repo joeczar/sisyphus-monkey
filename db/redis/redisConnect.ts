89cf54b41bf557f1d2bc48af61fe255e53a87f62
Joe Czarnecki <joe.czar@outlook.com>
2024-03-17 12:57:42 +0100

    chore: added connection check to redisConnect

--- db/redis/redisConnect.ts
+++ db/redis/redisConnect.ts
@@ -1,23 +1,26 @@
 import { redisConfig, type RedisConfig } from './redisConfig';
 import RedisClient, { type Redis } from 'ioredis';
 
+let hasConnected = false;
 export const redisConnect = (redisConfig: RedisConfig) => {
   const redisClient = new RedisClient(redisConfig);
 
   console.log('Redis client created.');
+  if (!hasConnected) {
+    redisClient.on('connect', () => {
+      console.log('Redis connection established.');
+      hasConnected = true;
+    });
 
-  redisClient.on('connect', () => {
-    console.log('Redis connection established.');
-  });
+    redisClient.on('error', (error) => {
+      console.error('Error connecting to Redis:', error);
+      throw error;
+    });
 
-  redisClient.on('error', (error) => {
-    console.error('Error connecting to Redis:', error);
-    throw error;
-  });
-
-  redisClient.on('close', () => {
-    console.log('Connection to Redis closed.');
-  });
+    redisClient.on('close', () => {
+      console.log('Connection to Redis closed.');
+    });
+  }
 
   console.log('Redis client initialized.');
 
