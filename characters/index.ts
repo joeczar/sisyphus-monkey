5d89768cb9b55c0fa63e9decf213265d1437074e
Joe Czarnecki <joe.czar@outlook.com>
2024-03-22 17:57:18 +0100

    debug: new observeable debugging

--- characters/index.ts
+++ characters/index.ts
@@ -40,7 +40,8 @@ async function initializeChars() {
   process.stdout.write(prompt);
   for await (const line of console) {
     if (line) {
-      await getAndParsePackets();
+      // await getAndParsePackets();
+      charsState.addToTotalChars(1);
     }
   }
 }
