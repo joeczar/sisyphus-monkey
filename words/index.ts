5d256e0fc9e4207c1b4eec38c60d74aca0178aa0
Joe Czarnecki <joe.czar@outlook.com>
2024-03-23 12:50:23 +0100

    debug: shutdown

--- words/index.ts
+++ words/index.ts
@@ -39,7 +39,7 @@ const initializeWords = async () => {
   await wordsState.setIsReady(true);
   console.log('Words server is ready');
 
-  handlePackets();
+  await handlePackets();
   wordsState.subscribeToChannel('channel:chars', async (message) => {
     console.log('Received message from chars:', message);
     const parsedMessage = await safeParseJson(message);
