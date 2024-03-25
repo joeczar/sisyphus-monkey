e0464113b1977b56e30b151f68cfdd6ce398825c
Joe Czarnecki <joe.czar@outlook.com>
2024-03-25 19:15:26 +0100

    chore(char): troubleshooting packet saving

--- server/CharServer.ts
+++ server/CharServer.ts
@@ -1,8 +1,5 @@
-import { getAndSortFiles } from '../characters/readAndSavePackets';
-import { charsState } from '../state/CharsState';
 import { packetRoutes } from './charsRoutes/packetRoutes';
 import { ServerBase } from './ServerBase';
-import { Hono } from 'hono';
 
 export class CharServer extends ServerBase {
   constructor() {
